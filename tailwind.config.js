c58fd6a60d1f911964883a7bf418fc514c9b3f2e
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-04-26 20:28:20 -0300

    style: added base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors:{
-      'green': '#B9FF66',
+    colors: {
+      green: "#B9FF66",
+      black: "#000000",
+      white: "#FFFFFF",
+      grey: "#F3F3F3",
+      darkGrey: "#191A23",
     },
     extend: {
       spacing: {
-        '37.5': '600px',
-        '19,375': '310px',
+        37.5: "600px",
+        "19,375": "310px",
+      },
+      bgColor: {
+        bggreen: "bg-green",
+        bgdarkGrey: "bg-grey",
+        bgwhite: "bg-white",
       },
     },
   },
