201bb3f0d7d96ef36bbc11d268dede7399ffd82f
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2023-04-26 15:52:58 -0300

    Refactor: Adjust in card component

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -7,19 +7,19 @@ interface ServiceCardProps {
    bgColor: string; 
 }
 
-function ServiceCard (props: ServiceCardProps) {
+export const ServiceCard = (props: ServiceCardProps) => {
    return (
-      <div className="w-37.5 h-19.375  flex grid-cols-2">
+
+      <div>
+         <h1 className={`p-2 ${props.titleBg}`}>{props.title}</h1>
+         <img src={props.icon} alt="Service card icon"/>
+         <p className={props.iconDescColor}>Learn more</p>
+         
          <div>
-            <h1 className={`p-2 ${props.titleBg}`}>{props.title}</h1>
-            <img src={props.icon} alt="Service card icon"/>
-            <p className={props.iconDescColor}>Learn more</p>
-         </div>
-         <div>
-            <img src={props.imgService} alt="Service card image" />
-         </div>
+         <img src={props.imgService} alt="Service card image" />
       </div>
+      </div>
+
    );
 }
 
-export default ServiceCard;
