00b05fe0afa7fac2536308514b57c9e7c6a73fc4
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-04-26 20:26:18 -0300

    feat: button component

---
+++ src/components/button/button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface ButtonProps {
+  children?: React.ReactNode;
+  bgColor: string;
+}
+
+const Button = ({ children, bgColor }: ButtonProps) => {
+  const btnClassName = `rounded-2xl border-2 border-black py-5 px-9 h-16 font-primary text-center ${bgColor}`;
+
+  return <button className={btnClassName}>{children}</button>;
+};
+
+export default Button;
