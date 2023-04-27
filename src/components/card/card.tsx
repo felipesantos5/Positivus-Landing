6e14a2edd055cc9cd6048902491717947091e9ec
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2023-04-27 15:45:36 -0300

    style: card component changes

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,25 +1,33 @@
+import React from "react";
 interface ServiceCardProps {
-   title: string;
+   bgColor: string; 
+   title1: string;
+   title2: string;
    titleBg: string;
    icon: string;
    iconDescColor: string;
    imgService: string;
-   bgColor: string; 
 }
 
 export const ServiceCard = (props: ServiceCardProps) => {
    return (
-
-      <div>
-         <h1 className={`p-2 ${props.titleBg}`}>{props.title}</h1>
-         <img src={props.icon} alt="Service card icon"/>
-         <p className={props.iconDescColor}>Learn more</p>
-         
-         <div>
-         <img src={props.imgService} alt="Service card image" />
+      <div className={`mt-4 ml-4 w-37.5 h-19.375 border border-darkGrey drop-shadow-cardShadow rounded-cardRadius ${props.bgColor}`}>
+         <div className="flex flex-row grid-cols-2">
+            <div className="mt-12 ml-12 flex flex-col gap-24">
+               <div className="">
+                  <h1 className={` inline-block p-1 text-3xl rounded-lg ${props.titleBg}`}>{props.title1}</h1>
+                  <h1 className={` inline-block p-1 text-3xl rounded-lg ${props.titleBg}`}>{props.title2}</h1>
+               </div>
+               <div className="flex flex-row items-center gap-3.5">
+                  <img src={props.icon} alt="Service card icon"/>
+                  <p className={props.iconDescColor}>Learn more</p>
+               </div>
+            </div>
+            <div className="mr-12 mt-10">
+               <img src={props.imgService} alt="Service card image" className="w-72 h-56" />
+            </div>
+         </div>
       </div>
-      </div>
-
    );
 }
 
