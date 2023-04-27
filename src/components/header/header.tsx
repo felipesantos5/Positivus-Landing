1691c278586ffb283684f2ea0f715d95b2dddc9b
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-04-26 21:34:51 -0300

    refactor: changing the component anchor

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import logo from "../../public/img/logo/logo.svg";
 import Button from "../button/button";
+import { Anchor } from "./anchor";
 
 function Header() {
   return (
-    <div className="flex ">
+    <div className="flex justify-around">
       <img src={logo} alt="logo Positivus" />
-      <div>
-        <nav>
-          <a href="#">About us</a>
-          <a href="#">Services</a>
-          <a href="#">Use Cases</a>
-          <a href="#">Pricing</a>
-          <a href="#">Blog</a>
+      <div className="flex items-center">
+        <nav className="flex gap-10">
+          <Anchor>About us</Anchor>
+          <Anchor>Services</Anchor>
+          <Anchor>Use Cases</Anchor>
+          <Anchor>Pricing</Anchor>
+          <Anchor>Blog</Anchor>
         </nav>
-        <Button bgColor={"bg-white"}>Request a quote</Button>\
+        <Button bgColor={"bg-white"}>Request a quote</Button>
       </div>
     </div>
   );
