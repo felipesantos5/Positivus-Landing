ee43bb0d6a17a42237d0fb2972308d1fc7f02357
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2023-04-28 21:10:34 -0300

    improvement: continuing the app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 // import { useState } from "react";
 import FirstPage from "./components/introductionpage/fistpage";
 import Header from "./components/header/header";
+import CardPage from "./components/cardPage/cardPage";
 
 function App() {
   return (
     <div>
       <Header></Header>
       <FirstPage></FirstPage>
+      <CardPage></CardPage>
     </div>
   );
 }
