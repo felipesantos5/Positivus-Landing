a654d4079b53d070274c1cb940e9563fa624e880
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-05-02 21:35:24 -0300

    improvement: changing the paragraph component

--- src/components/introductionpage/fistpage.tsx
+++ src/components/introductionpage/fistpage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "../button/button";
 import ilustration from "../../public/img/firstPage/Illustration.png";
 import Marks from "../marks/brands";
+import Paragraph from "../paragraph/paragraph";
 
 const firstPage = () => {
   return (
@@ -9,7 +10,9 @@ const firstPage = () => {
       <div className="flex gap-24">
         <div className="flex flex-col gap-8">
           <h1 className="text-6xl w-531">Navigating the digital landscape for success</h1>
-          <p className="w-96">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
+          <Paragraph width={498}>
+            Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.
+          </Paragraph>
           <Button bgColor={"bg-darkGrey"} width="264">
             Book a consultation
           </Button>
