3c672d18281fbd89e3d556c7a4bf4744657c79c8
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-05-09 13:20:55 -0300

    fix: fixing the interface props

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 interface ButtonProps {
-  children?: React.ReactNode;
+  children: React.ReactNode;
   bgColor: string;
   width?: string;
+  textColor: string;
 }
 
 const Button = (props: ButtonProps) => {
-  const btnClassName = `rounded-2xl border-2 border-black py-5 px-9 h-16 font-primary text-center ${props.bgColor} ${props.width && `w-${props.width}`}`;
+  const btnClassName = `block rounded-2xl border-2 border-black py-5 px-9 h-16 font-primary text-center ${props.bgColor} ${props.textColor} ${props.width}`;
 
   return <button className={btnClassName}>{props.children}</button>;
 };
