6bf2d93e06b494d3ef7442287c927f814164a8b1
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-05-12 18:41:47 -0300

    style: correting the import and changing styles

--- src/components/introductionpage/fistpage.tsx
+++ src/components/introductionpage/fistpage.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Button from "../button/button";
 import ilustration from "../../public/img/firstPage/Illustration.png";
-import Marks from "../marks/brands";
+import Marks from "../brands/brands";
 import Paragraph from "../paragraph/paragraph";
 
 const firstPage = () => {
   return (
-    <div className="">
-      <div className="flex gap-24">
+    <div className="flex-row">
+      <div className="flex justify-between">
         <div className="flex flex-col gap-8">
-          <h1 className="text-40 w-531">Navigating the digital landscape for success</h1>
-          <Paragraph width="580">
+          <h1 className="text-60 w-531 font-medium">Navigating the digital landscape for success</h1>
+          <Paragraph width="w-498">
             Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.
           </Paragraph>
           <Button bgColor={"bg-darkGrey"} width="w-264" textColor="text-white">
