02cc3fe42e1d0c16325b2b0a3635e81ee8e42b08
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2023-05-12 19:35:19 -0300

    refactor: changed the prop names

--- src/components/title/title.tsx
+++ src/components/title/title.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 interface SectionTitleProps {
-  title: string;
-  titleDesc: string;
+  children: string;
+  desc: string;
   widthDesc: string;
 }
 
 export const SectionTitle = (props: SectionTitleProps) => {
   return (
     <div className="mb-20 mt-36 flex flex-row items-center gap-10">
-      <h2 className="p-2 font-bold text-40 bg-green rounded-lg leading-77">{props.title}</h2>
-      <p className={`text-lg ${props.widthDesc}`}>{props.titleDesc}</p>
+      <h2 className="p-2 font-bold text-40 bg-green rounded-lg leading-77">{props.children}</h2>
+      <p className={`text-lg ${props.widthDesc}`}>{props.desc}</p>
     </div>
   );
 };
