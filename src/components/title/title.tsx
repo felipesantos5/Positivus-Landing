b467d66f46bd25fc03c26e0879bab7736681e140
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2023-05-18 10:45:11 -0300

    fix: correcting props

--- src/components/title/title.tsx
+++ src/components/title/title.tsx
@@ -4,13 +4,14 @@ import Paragraph from "../paragraph/paragraph";
 interface SectionTitleProps {
   title: string;
   desc: string;
+  widthDesc: string;
 }
 
 export const SectionTitle = (props: SectionTitleProps) => {
   return (
     <div className="mb-20 mt-36 flex flex-row items-center gap-10">
-      <h2 className="p-2 font-bold text-40 bg-green rounded-lg leading-77">{props.title}</h2>
-      <Paragraph width={"w-580"}>{props.desc}</Paragraph>
+      <h2 className="p-1 font-bold text-40 bg-green rounded-lg leading-77">{props.title}</h2>
+      <Paragraph width={props.widthDesc}>{props.desc}</Paragraph>
     </div>
   );
 };
