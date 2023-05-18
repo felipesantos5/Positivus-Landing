121c2edfaf5481f6cf7eb3311ce2ef492653bc5c
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2023-05-18 10:41:41 -0300

    refactor: unused variable removed

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import darkIcon from "../../public/img/serviceCardImg/darkIcon.svg";
 import whiteIcon from "../../public/img/serviceCardImg/whiteIcon.svg";
 interface ServiceCardProps {
-  variant: 'default' | 'white' | 'dark' | 'green';
+  variant: 'white' | 'dark' | 'green';
   children1: string;
   children2: string;
   imgService: string;
@@ -15,8 +15,6 @@ export const ServiceCard = (props: ServiceCardProps) => {
   const style = "border border-darkGrey drop-shadow-cardShadow rounded-cardRadius";
 
   switch (variant) {
-    case 'default':
-      return <DefaultCard defaultStyle={style} {...props}/>;
     case 'white':
       return <WhiteCard defaultStyle={style} {...props}/>;
     case 'dark':
@@ -25,39 +23,15 @@ export const ServiceCard = (props: ServiceCardProps) => {
       return <GreenCard defaultStyle={style} {...props}/>;
   }
 };
-export const DefaultCard = (props: ServiceCardProps) => {
-  const {children1, children2, imgService, widthImage, defaultStyle} = props;
-
-  return(
-    <div className={`${defaultStyle}`}>
-      <div className="flex grid-cols-2 justify-between mx-12 my-12">
-        <div className="flex flex-col gap-20">
-          <div className="">
-            <h1 className={`font-medium text-30 inline-block p-1 rounded-lg text-black`}>{props.children1}</h1>
-            <h2 className={`font-medium text-30 inline-block p-1 rounded-lg text-black`}>{props.children2}</h2>
-          </div>
-          <div className="flex flex-row items-center gap-3.5">
-            <img src= {darkIcon} alt="Service card icon" />
-            <p>Learn more</p>
-          </div>
-        </div>
-        <div>
-          <img src={props.imgService} alt="Service card image" className={`${props.widthImage}`} />
-        </div>
-      </div>
-    </div>
-  )
-};
 
 export const WhiteCard = (props: ServiceCardProps) => {
-  const {children1, children2, imgService, widthImage, defaultStyle} = props;
   
   return (
-    <div className={`${defaultStyle} bg-white`}>
+    <div className={`${props.defaultStyle} bg-white`}>
     <div className="flex grid-cols-2 justify-between mx-12 my-12 items-center">
       <div className="flex flex-col gap-20">
         <div className="">
-          <h1 className={`font-medium text-30 inline-block p-1 rounded-lg text-black bg-green`}>{props.children1}</h1>
+          <h1 className="font-medium text-30 inline-block p-1 rounded-lg text-black bg-green">{props.children1}</h1>
           <h2 className={`font-medium text-30 inline-block p-1 rounded-lg text-black bg-green`}>{props.children2}</h2>
         </div>
         <div className="flex flex-row items-center gap-3.5">
@@ -74,10 +48,9 @@ export const WhiteCard = (props: ServiceCardProps) => {
 };
 
 export const DarkCard = (props: ServiceCardProps) => {
-  const {children1, children2, imgService, widthImage, defaultStyle} = props;
 
   return (
-    <div className={`${defaultStyle} bg-darkGrey`}>
+    <div className={`${props.defaultStyle} bg-darkGrey`}>
     <div className="flex grid-cols-2 justify-between mx-12 my-12 items-center">
       <div className="flex flex-col gap-20">
         <div className="">
@@ -98,10 +71,9 @@ export const DarkCard = (props: ServiceCardProps) => {
 };
 
 export const GreenCard = (props: ServiceCardProps) => {
-  const {children1, children2, imgService, widthImage, defaultStyle} = props;
 
   return (
-    <div className={`${defaultStyle} bg-green`}>
+    <div className={`${props.defaultStyle} bg-green`}>
     <div className="flex grid-cols-2 justify-between mx-12 my-12 items-center">
       <div className="flex flex-col gap-20">
         <div className="">
