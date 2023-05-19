693949939fffff5e09fdc863b36e06e717c41af2
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-05-19 11:08:31 -0300

    feat: create toggle card function

--- src/components/workingProcess/card.tsx
+++ src/components/workingProcess/card.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Paragraph from "../paragraph/paragraph";
-import plus from "../../public/img/workingProcess/plus.svg";
+import plusImage from "../../public/img/workingProcess/plus.png";
+import minusImage from "../../public/img/workingProcess/minus.png";
 import line from "../../public/img/workingProcess/Line.svg";
 
 interface cardWorking {
@@ -11,20 +12,41 @@ interface cardWorking {
 }
 
 export const CardWorking = (props: cardWorking) => {
+  const [image, setImage] = useState<string>(plusImage);
+  const [hiddenDiv, sethiddenDiv] = useState(true);
+
+  const toggleClasse = () => {
+    sethiddenDiv(!hiddenDiv);
+  };
+
+  function changeImage() {
+    if (image === plusImage) {
+      setImage(minusImage);
+    } else {
+      setImage(plusImage);
+    }
+  }
+
+  function toggleCard() {
+    toggleClasse();
+    changeImage();
+  }
+
   return (
     <div className="border border-darkGrey drop-shadow-cardShadow rounded-cardRadius bg-white mb-8 px-16 py-10 flex flex-col">
-      <div className="flex justify-between mb-7">
+      <div className="flex justify-between">
         <div className="flex gap-9 items-center">
           <h2 className="text-60 font-medium">{props.number}</h2>
           <h2 className="text-30 font-medium h-38">{props.title}</h2>
         </div>
         {
-          <button className="">
-            <img src={plus} alt="button" />
+          <button className="" onClick={() => toggleCard()}>
+            <img src={image} alt="button" />
           </button>
         }
       </div>
-      <div className="">
+      {/* hidden mt-7 */}
+      <div className={`minha-div ${hiddenDiv ? "hidden" : ""}`}>
         <img src={line} alt="line" />
         <Paragraph width={""} children={"lorem yeqrwfrqewrqwrfew"}></Paragraph>
       </div>
