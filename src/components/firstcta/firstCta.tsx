cdfb7e845b5a181e22550050978bbe9e71a86908
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2023-05-19 11:24:33 -0300

    style: adjusted the styles

--- src/components/firstcta/firstCta.tsx
+++ src/components/firstcta/firstCta.tsx
@@ -5,8 +5,8 @@ import Illustration from "../../public/img/firstCta/Illustration.png";
 
 const FirstCta = () => {
   return (
-    <div className="flex justify-between bg-grey rounded-cardRadius mt-32 h-347 ml-14 mr-36">
-      <div className="flex flex-col gap-6 justify-center">
+    <div className="flex justify-between bg-grey rounded-cardRadius mt-32 h-347">
+      <div className="flex flex-col gap-6 justify-center ml-16">
         <h1 className="text-30 font-medium">Let’s make things happen</h1>
         <Paragraph width={"w-498"} children={"Contact us today to learn more about how our digital marketing services can help your business grow and succeed online."}></Paragraph>
         <Button bgColor={"bg-darkGrey"} width="w-288" textColor="text-white">
@@ -14,7 +14,7 @@ const FirstCta = () => {
         </Button>
       </div>
       <div>
-        <img src={Illustration} alt="illustration" className="relative top-23 left-20" />
+        <img src={Illustration} alt="illustration" className="relative top-23 mr-32" />
       </div>
     </div>
   );
