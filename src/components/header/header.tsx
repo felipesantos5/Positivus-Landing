7a0438103f8942fadc3e54c4122d49397565de46
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-05-31 14:39:23 -0300

    refactor: refactoring header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import logo from "../../public/img/logo/logo.svg";
-import Button from "../button/button";
+import Button from "../default/button/button";
 import { Anchor } from "./anchor";
 
 function Header() {
   return (
-    <div className="flex justify-between mt-10 mb-16">
+    <header className="flex justify-between mt-10 mb-16">
       <img src={logo} alt="logo Positivus" />
       <div className="flex items-center">
         <nav className="flex gap-10 mr-10">
@@ -19,7 +19,7 @@ function Header() {
           Request a quote
         </Button>
       </div>
-    </div>
+    </header>
   );
 }
 
